9e1324a77705ea86093d5131baad14ebfc9d3a66
jero10lf <jero10lf@gmail.com>
2021-05-23 13:07:49 +0200

    Add list favorites

--- src/js/02-data.js
+++ src/js/02-data.js
@@ -69,4 +69,44 @@ function renderSeries() {
   }
 
   seriesListElement.innerHTML = htmlCode;
+  listenClickedSeries();
+}
+function addFavorites(event) {
+  const clickedfavorite = event.currentTarget;
+  const clickedSerie = parseInt(clickedfavorite.id);
+  const serieFound = series.find((serie) => serie.id === clickedSerie);
+  const favoriteFound = favorites.findIndex(
+    (favorite) => favorite.id === clickedSerie
+  );
+
+  if (favoriteFound === -1) {
+    favorites.push(serieFound);
+  } else {
+    favorites.splice(favoriteFound, 1);
+  }
+  renderSeries();
+  renderFavorites();
+}
+
+function listenClickedSeries() {
+  const liElements = document.querySelectorAll(".js-serie");
+  for (const liElement of liElements) {
+    liElement.addEventListener("click", addFavorites);
+  }
+}
+
+function renderFavorites() {
+  let htmlCode = "";
+  for (const elementFavorite of favorites) {
+    htmlCode += `<li id ="${elementFavorite.id}" class="js-serieFav li__fav"  >`;
+    htmlCode += `<h3 class="liTitle">${elementFavorite.name}</h3>`;
+    if (elementFavorite.image === null) {
+      htmlCode += `<img class="serie__img" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" title="${elementFavorite.name}" alt="${elementFavorite.name} cover not available"/>`;
+    } else {
+      htmlCode += `<img class="serie__img" src="${elementFavorite.image.medium}" title="${elementFavorite.name}" alt="${elementFavorite.name}  cover"/>`;
+    }
+    htmlCode += "</li>";
+  }
+
+  favoritesListElement.innerHTML = htmlCode;
 }
